d98bf6391c396ca04c266ec2f12e71abe1df9518
bripapp <brianna.papparotto@gmail.com>
2021-04-23 18:27:19 -0400

    final push

--- etl.sql
+++ etl.sql
@@ -1,5 +1,4 @@
-drop table platforms;
-
+/*create platforms table*/
 create table platforms (
   "Title" varchar (200),
   "Year" varchar (4),
@@ -21,6 +20,28 @@ create table platforms (
   "Language 2" varchar (50)
 );
 
+drop table blockbusters;
+
+/*create blockbusters table*/
+create table blockbusters (
+  "Release Year" varchar (4),
+  "Rank in Year" varchar (4),
+  "IMDb Rating" float,
+  "MPAA Rating" varchar (10),
+  "Title" varchar (200),
+  "Film Budget" bigint,
+  "Runtime" int,
+  "Domestic Distributor" varchar (50),
+  "Worldwide Gross" bigint,
+  "Domestic Gross" bigint,
+  "Genre 1" varchar (50),
+  "Genre 2" varchar (50),
+  "Genre 3" varchar (50)
+);
+
+
+
+/* Sample Query to select data from platforms*/
 SELECT * from platforms 
 where "Netflix" = 1
 order by "Title"; 
